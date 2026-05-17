0623d11b4646063144a3843d5e5915965f6301ff
Ciccio <ccciiccio@gmail.com>
2026-05-17 15:43:28 +0200

    Fix definitivo entry point con index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import { registerRootComponent } from 'expo';
+import App from './App'; // Assicurati solo che se il tuo file si chiama app.js con la minuscola, scrivi './app'
+
+registerRootComponent(App);
